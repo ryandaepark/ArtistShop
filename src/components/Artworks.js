70d224878255c4269a00ee0edc37cb97c80abb48
ryandaepark <ryanp0209@gmail.com>
2024-01-28 19:15:10 -0800

    MiniMe gallery added

--- src/components/Artworks.js
+++ src/components/Artworks.js
@@ -1,116 +1,153 @@
-import img1 from '../RyanAssets/artist.jpg'
-import img2 from '../RyanAssets/cheetah.jpg'
-import img3 from '../RyanAssets/burger.jpg'
-import img4 from '../RyanAssets/cube.jpg'
-import img5 from '../RyanAssets/girl.jpg'
-import img6 from '../RyanAssets/coffee.jpg'
-import gif from '../RyanAssets/Shop2.gif'
+import img1 from '../RyanAssets/Alltornup.PNG'
+import img2 from '../RyanAssets/Alltornup2.png'
+import img3 from '../RyanAssets/Alltornup3.png'
+import img4 from '../RyanAssets/Alltornup4.png'
+import img5 from '../RyanAssets/Icanbegreat.JPG'
+import img6 from '../RyanAssets/playinggames.jpeg'
+import img7 from '../RyanAssets/trustyourgut.jpeg'
+import img8 from '../RyanAssets/prolongingtheinevitable.png'
+import img9 from '../RyanAssets/joy.jpeg'
+import img10 from '../RyanAssets/Close2NoOne.jpeg'
+import img11 from '../RyanAssets/Organization.jpg'
+import img12 from '../RyanAssets/Selfie.jpeg'
+import img13 from '../RyanAssets/Contemplation.jpeg'
+import img14 from '../RyanAssets/Dont Beat Yourself Up.png'
+import img15 from '../RyanAssets/Truman Show.png'
+import img16 from '../RyanAssets/I Control My Reality.JPG'
+import img17 from '../RyanAssets/Coldest Winter.jpeg'
+import img18 from '../RyanAssets/Self Love.jpeg'
+import img19 from '../RyanAssets/Night Routine.jpg'
+import img20 from '../RyanAssets/Old Timer.jpg'
+import img21 from '../RyanAssets/Solitude.PNG'
+import img22 from '../RyanAssets/materialism.png'
+import img23 from '../RyanAssets/Moody.png'
+import img24 from '../RyanAssets/Microscopic.jpg'
+import img25 from '../RyanAssets/Ellipses.JPG'
 
 export default [
     {
-    img: img3, 
-    title:"Burger", 
-    type: "MiniMe",
-    }, 
-    {
-    img: img3, 
-    title:"Burger", 
-    type: "MiniMe",
-    }, 
-    {
-    img: img3, 
-    title:"Burger", 
-    type: "MiniMe",
-    }, 
-    {
-    img: img3, 
-    title:"Burger", 
-    type: "MiniMe",
-    }, 
-    {
-    img: img3, 
-    title:"Burger", 
-    type: "MiniMe",
-    }, 
-    , 
-    {
-    img: img3, 
-    title:"Burger", 
-    type: "MiniMe",
-    }, 
-    {
-    img: img3, 
-    title:"Burger", 
-    type: "MiniMe",
-    }, 
-    {
-    img: img3, 
-    title:"Burger", 
-    type: "MiniMe",
-    }, 
-    {
-    img: img3, 
-    title:"Burger", 
-    type: "MiniMe",
-    }, 
-    {
     img: img1, 
-    title:"Artist", 
-    type: "Illustrations",
+    title:"All Torn Up", 
+    type: "MiniMe",
     }, 
     {
     img: img2, 
-    title:"Cheetah", 
-    type: "Illustrations",
+    title:"All Torn Up 2", 
+    type: "MiniMe",
     }, 
     {
     img: img3, 
-    title:"Burger", 
-    type: "Illustrations",
+    title:"All Torn Up 3", 
+    type: "MiniMe",
     }, 
     {
     img: img4, 
-    title:"Cube", 
-    type: "Illustrations",
+    title:"All Torn Up 4", 
+    type: "MiniMe",
     }, 
     {
     img: img5, 
-    title:"Girl", 
-    type: "Illustrations",
+    title:"I Can Be Great", 
+    type: "MiniMe",
     }, 
     {
     img: img6, 
-    title:"Coffee", 
-    type: "Illustrations",
+    title:"Playing Games", 
+    type: "MiniMe",
+    }, 
+    {
+    img: img7, 
+    title:"Trust Your Gut", 
+    type: "MiniMe",
     },
     {
-    img: gif, 
-    title:"Gif", 
-    type: "Animations",
-    }, 
+    img: img8, 
+    title:"Prolonging The Inevitable", 
+    type: "MiniMe",
+    },
     {
-    img: gif, 
-    title:"Gif", 
-    type: "Animations",
-    }, 
+    img: img9, 
+    title:"Joy", 
+    type: "MiniMe",
+    },
     {
-    img: gif, 
-    title:"Gif", 
-    type: "Animations",
-    }, 
+    img: img10, 
+    title:"Close 2 No One", 
+    type: "MiniMe",
+    },
     {
-    img: gif, 
-    title:"Gif", 
-    type: "Animations",
-    }, 
+    img: img11, 
+    title:"Organization", 
+    type: "MiniMe",
+    },
     {
-    img: gif, 
-    title:"Gif", 
-    type: "Animations",
-    }, 
+    img: img12, 
+    title:"Selfie", 
+    type: "MiniMe",
+    },
     {
-    img: gif, 
-    title:"Gif", 
-    type: "Animations",
-    }
+    img: img13, 
+    title:"Contemplation", 
+    type: "MiniMe",
+    },
+    {
+    img: img14, 
+    title:"Don't Beat Yourself Up", 
+    type: "MiniMe",
+    },
+    {
+    img: img15, 
+    title:"Truman Show", 
+    type: "MiniMe",
+    },
+    {
+    img: img16, 
+    title:"I Control My Reality", 
+    type: "MiniMe",
+    },
+    {
+    img: img17, 
+    title:"Coldest Winter", 
+    type: "MiniMe",
+    },
+    {
+    img: img18, 
+    title:"Self Love", 
+    type: "MiniMe",
+    },
+      {
+    img: img19, 
+    title:"Night Routine", 
+    type: "MiniMe",
+    },
+    {
+    img: img20, 
+    title:"Old Timer", 
+    type: "MiniMe",
+    },
+    {
+    img: img21, 
+    title:"Solitude", 
+    type: "MiniMe",
+    },
+    {
+    img: img22, 
+    title:"Materialism", 
+    type: "MiniMe",
+    },
+    {
+    img: img23, 
+    title:"Moody", 
+    type: "MiniMe",
+    },
+    {
+    img: img24, 
+    title:"Microscopic", 
+    type: "MiniMe",
+    },
+    {
+    img: img25, 
+    title:"Ellipses", 
+    type: "MiniMe",
+    },
 ]
